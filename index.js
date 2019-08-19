0fb54ed0551d5f64c609650c9b1f876aad4d8a31
Katie Rose <katiefelten@gmail.com>
2019-08-19 16:40:50 -0400

    :white_check_mark: guided project complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
+const restricted = require("./auth/restricted-middleware.js");
 
 const server = express();
 
@@ -11,12 +13,12 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send("It's alive!");
-});
-
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   let user = req.body;
+
+  // hash password
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -27,16 +29,16 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
@@ -44,12 +46,26 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+// can only be accessed by clients with valid credentials
+server.get("/api/users", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+// can only be accessed by clients with valid credentials
+server.get("/", restricted, (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post("/hash", (req, res) => {
+  const password = req.body.password;
+
+  const hash = bcrypt.hashSync(password, 12);
+
+  res.status(200).json({ password, hash });
 });
 
 const port = process.env.PORT || 5000;
